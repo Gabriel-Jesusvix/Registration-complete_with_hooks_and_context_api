746904c4dbfefa8e334b260b70adb4235b9fb08d
gabriel.jesus <62946928+Gabriel-Jesusvix@users.noreply.github.com>
2021-06-08 20:25:27 -0300

    service user, and render the view

--- src/Pages/UserList/index.js
+++ src/Pages/UserList/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, FlatList, Text} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {ListItem, Avatar} from 'react-native-elements';
+
+import users from '../../service/user';
 
 export default function UserList() {
+  const {navigate} = useNavigation();
+
+  function renderItemUser({item: user}) {
+    return (
+      <ListItem
+        key={user.id}
+        bottomDivider
+        onPress={() => navigate('UserForm')}>
+        <Avatar source={{uri: user.avatarUrl}} />
+        <ListItem.Content>
+          <ListItem.Title>{user.name}</ListItem.Title>
+          <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    );
+  }
+
   return (
     <View>
-      <Text>UserList</Text>
+      <FlatList
+        data={users}
+        keyExtractor={user => user.id.toString()}
+        renderItem={renderItemUser}
+      />
     </View>
   );
 }
